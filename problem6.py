c7b758ec697a169e8fa586984752f63e5f913d7b
Garret Tonra <gtonra89@gmail.com>
2017-09-23 16:36:44 +0100

    added to problem 6

---
+++ problem6.py
@@ -0,0 +1,9 @@
+#Write a function that returns the largest and smallest elements in a list.
+list1 = ['me','xyz', 'zara', 'abc','blllllllllllllllllllaaaaaaaaaah']
+
+def MinandMaxElement(a):
+    print("Max value element : ",max(a))
+    print("min value element : ",min(a))
+
+MinandMaxElement(list1)
+
