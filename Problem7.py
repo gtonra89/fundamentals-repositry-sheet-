0c1c96aede1ebc0cc8be09eb577bdb39aa59a7b3
Garret Tonra <gtonra89@gmail.com>
2017-09-23 17:27:26 +0100

    made function work for problem 7

--- Problem7.py
+++ Problem7.py
@@ -1,14 +1,17 @@
 # Program to check if a string is a palindrome or not
 
 # change this value for a different output
-myString = 'abaBA'
+def palTest(str):
 # make it suitable for caseless comparison
-myString = myString.casefold()
+    myString = str.casefold()
 # reverse the string
-reverseString = reversed(myString)
+    reverseString = reversed(myString)
 # check if the string is equal to its reverse
-print("string to check :",myString)
-if list(myString) == list(reverseString):
-   print("It is palindrome")
-else:
-   print("It is not palindrome")
+    print("string to check :",myString)
+    if list(myString) == list(reverseString):
+        print("It is palindrome")
+    else:
+        print("It is not palindrome")
+
+myString = 'abaBA' #declare string
+palTest(myString)  #call function pass string
