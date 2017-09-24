74422d99fd4df4cf7a9728707de785de7385f272
Garret Tonra <gtonra89@gmail.com>
2017-09-24 21:20:48 +0100

    added to problem 10

---
+++ Problem10.py
@@ -0,0 +1,7 @@
+string='abcdefghijklmnopqrstuvwxyz'
+
+def rev(str):
+    print("The word to reverse is :",str)
+    reversed_string = string[::-1]
+    print("The reversed string is :",reversed_string)
+rev(string)  
